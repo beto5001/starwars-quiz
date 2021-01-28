4ca441d5389a407699a58f38710ad09b5ee19ec0
Alberto Ferreira da Silva <alberto.abfs@hotmail.com>
2021-01-28 00:28:20 -0300

    Página de quiz

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
@@ -26,6 +28,10 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('RETORNO DO STATE', name, setName);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -35,7 +41,23 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infoDoEvento) {
+              infoDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz aí seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import styled from 'styled-components'
 
 const FooterWrapper = styled.footer`
